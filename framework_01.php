1530f24faf1d6dbbc0ee175cae4761ed29bd0728
IvanaDeAlba <ivanadanae.rda@gmail.com>
2017-01-30 12:43:30 -0600

    Add files via upload

---
+++ framework_01.php
@@ -0,0 +1,60 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>WATMARK</title>
+		<!-- Basic Page Needs -->
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<title>05</title>
+		<meta name="description" content=""/>
+		<meta name="keywords" content=""/>
+		<meta name="author" content="Ivana De Alba"/>
+		
+		<!-- Mobile Specific Metas -->
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+		
+		<!-- Fonts -->
+		
+		<!-- Css -->
+		<link rel="stylesheet" href="css/waw.css"/>
+		<link rel="stylesheet" href="css/style.css"/>
+		
+		<!-- Favicon -->
+		<link rel="icon" type="image/png" href="favicon.png"/>
+		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico"/>
+		<link rel="shortcut icon" type="image/ico" href="favicon.ico"/>
+		
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+		
+	</head>
+	<body>
+
+		<header>
+			<div class="container borderG">
+				<div class="row">
+					<h1 class="show text-center">WATMARK</h1>
+					<h1 class="showT text-center">WATMARK</h1>
+					<h1 class="showM text-center">WATMARK</h1>
+				</div>
+				<div class="row">
+					<div class="col four borderG borderH showM showT"> </div>
+					<div class="col four borderG borderH showM showT"> </div>
+					<div class="col four borderG borderH showM showT"> </div>
+					<div class="col four borderG borderH showM showT"> </div>
+					<div class="col four borderG borderH showM showT"> </div>
+					<div class="col four borderG borderH showM showT"> </div>
+					<div class="col four borderG borderH"> </div>
+					<div class="col four borderG borderH"> </div>
+					<div class="col four borderG borderH"> </div>	
+				</div>
+			</div>
+		</header>
+
+	</body>
+</html>
